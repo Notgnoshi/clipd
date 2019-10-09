b2244e065b972e7a6e0ebef79f58e0a9a2c4ebd7
Austin Gill <notgnoshi@gmail.com>
2019-10-09 17:39:37 -0600

    Check if callback is called only when there's an update

---
+++ tests/test_clipboard_listener.cpp
@@ -0,0 +1,43 @@
+#include "clipboard/clipboard_daemon.h"
+
+#include <chrono>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace std::chrono_literals;
+
+using ::testing::AtLeast;
+using ::testing::InSequence;
+using ::testing::Return;
+using ::testing::StrictMock;
+
+class MockClipboardDaemon : public Clipd::Clipboard::ClipboardDaemon
+{
+public:
+    MOCK_METHOD( std::string, getClipboardTextContents, (), ( const, override ) );
+    MOCK_METHOD( void, TextUpdateCallback, (const std::string&));
+};
+
+TEST( ClipboardListenerTests, TestUniqueCallbackCalls )
+{
+    MockClipboardDaemon listener;
+    // InSequence dummy;
+
+    listener.registerOnTextUpdate( delegate<void( const std::string& )>(
+        listener, &MockClipboardDaemon::TextUpdateCallback ) );
+
+    // Ensure that the clipboard listener will always return the constant value "v".
+    ON_CALL( listener, getClipboardTextContents() ).WillByDefault( Return( "v" ) );
+
+    EXPECT_CALL( listener, getClipboardTextContents() ).Times( AtLeast( 2 ) );
+    EXPECT_CALL( listener, TextUpdateCallback( "v" ) ).Times( 1 );
+
+    listener.start();
+
+    // Give the listener thread enough time to start up and process at least a few
+    std::this_thread::sleep_for( 5ms );
+
+    listener.stop();
+    listener.join();
+}
